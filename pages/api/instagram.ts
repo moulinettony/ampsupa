964b2fb6f6a534af9a5092604d263b1f6a8da702
Macbook <midolf.leesin@gmail.com>
2024-08-07 15:58:14 +0100

    Add AMP integration and Instagram API

--- pages/api/instagram.ts
+++ pages/api/instagram.ts
@@ -20,6 +20,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.status(200).json(data);
   } catch (error:any) {
     console.error('Error fetching Instagram posts:', error.message);
-    res.status(500).json({ error: 'Failed to fetch Instagram posts' });
+    res.status(500).json({ error });
   }
 }
