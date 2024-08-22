2c87e5652ec9696cd7c2b16803d0eb5790609650
Macbook <midolf.leesin@gmail.com>
2024-08-22 16:16:38 +0100

    add clarity

--- pages/tracklarity.tsx
+++ pages/tracklarity.tsx
@@ -23,6 +23,20 @@ const ExampleAMPPage = ({ clarityTrackingId }: PageProps) => {
             `,
           }}
         />
+
+        <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "nr9uu90bjk");
+              `,
+            }}
+          />
+
         <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
         <script
           type="application/json"
