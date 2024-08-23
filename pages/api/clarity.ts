948cfb8f4894359134e501a708e04d5a8b00d4b6
Macbook <midolf.leesin@gmail.com>
2024-08-23 14:00:18 +0100

    add clarity

--- pages/api/clarity.ts
+++ pages/api/clarity.ts
@@ -1,12 +1,16 @@
+// /pages/api/clarity.ts
 import { NextApiRequest, NextApiResponse } from 'next';
 
-interface ClarityResponse {
-  trackingId: string;
+interface ClarityData {
+  projectId: string;
+  trackingUrl: string;
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ClarityResponse>
-) {
-  res.status(200).json({ trackingId: 'nr9uu90bjk' });
+export default function handler(req: NextApiRequest, res: NextApiResponse<ClarityData>) {
+  const data: ClarityData = {
+    projectId: "nr9uu90bjk",
+    trackingUrl: `https://www.clarity.ms/tag/nr9uu90bjk`,
+  };
+
+  res.status(200).json(data);
 }
