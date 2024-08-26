84b83da6b1b5a42f66b060cfc117b4a8e55a26f9
Macbook <midolf.leesin@gmail.com>
2024-08-26 11:48:37 +0100

    add clarity

--- pages/api/clarity.ts
+++ pages/api/clarity.ts
@@ -1,6 +1,6 @@
 // pages/api/clarity.ts
 
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type ClarityData = {
   projectId: string;
@@ -11,10 +11,28 @@ type ResponseData = {
   items: ClarityData[];
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  // Add the CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "https://rolling.mydopweb.com");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  // Handle preflight (OPTIONS) request
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   const data: ClarityData = {
-    projectId: 'nr9uu90bjk',
-    trackingUrl: 'https://www.clarity.ms/tag/nr9uu90bjk',
+    projectId: "nr9uu90bjk",
+    trackingUrl: "https://www.clarity.ms/tag/nr9uu90bjk",
   };
 
   res.status(200).json({ items: [data] });
